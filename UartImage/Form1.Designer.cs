e7158bcf126d3cb58f4272703981bc5bc0f234cd
Vlad <33203144+1zaboy@users.noreply.github.com>
2020-01-13 07:42:31 +0300

    Добавьте файлы проекта.

---
+++ UartImage/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace UartImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxBaudRate = new System.Windows.Forms.TextBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelPort = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelError = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxDebug = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(660, 96);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(128, 23);
+            this.buttonConnect.TabIndex = 1;
+            this.buttonConnect.Text = "Подключить";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.ButtonConnect_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(660, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(86, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(660, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Порт";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialPort1_DataReceived);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(659, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Скорость порта";
+            // 
+            // textBoxBaudRate
+            // 
+            this.textBoxBaudRate.Location = new System.Drawing.Point(660, 70);
+            this.textBoxBaudRate.Name = "textBoxBaudRate";
+            this.textBoxBaudRate.Size = new System.Drawing.Size(128, 20);
+            this.textBoxBaudRate.TabIndex = 5;
+            this.textBoxBaudRate.Text = "115200";
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(663, 470);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(125, 23);
+            this.buttonClear.TabIndex = 6;
+            this.buttonClear.Text = "Обновить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.ButtonClear_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelPort,
+            this.toolStripStatusLabelError});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 508);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelPort
+            // 
+            this.toolStripStatusLabelPort.Name = "toolStripStatusLabelPort";
+            this.toolStripStatusLabelPort.Size = new System.Drawing.Size(101, 17);
+            this.toolStripStatusLabelPort.Text = "Состояние порта";
+            // 
+            // toolStripStatusLabelError
+            // 
+            this.toolStripStatusLabelError.Name = "toolStripStatusLabelError";
+            this.toolStripStatusLabelError.Size = new System.Drawing.Size(37, 17);
+            this.toolStripStatusLabelError.Text = "ждем";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(660, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Отключить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(660, 427);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Размер картинки";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(663, 443);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(48, 20);
+            this.textBoxX.TabIndex = 10;
+            this.textBoxX.Text = "640";
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(737, 444);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(50, 20);
+            this.textBoxY.TabIndex = 11;
+            this.textBoxY.Text = "480";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(717, 448);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "X";
+            // 
+            // button2
+            // 
+            this.button2.Image = global::UartImage.Properties.Resources.refresh24;
+            this.button2.Location = new System.Drawing.Point(753, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 36);
+            this.button2.TabIndex = 13;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBoxDebug
+            // 
+            this.textBoxDebug.Location = new System.Drawing.Point(660, 167);
+            this.textBoxDebug.MaxLength = 100000;
+            this.textBoxDebug.Multiline = true;
+            this.textBoxDebug.Name = "textBoxDebug";
+            this.textBoxDebug.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxDebug.Size = new System.Drawing.Size(127, 257);
+            this.textBoxDebug.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 530);
+            this.Controls.Add(this.textBoxDebug);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.textBoxBaudRate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "UartImage";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxBaudRate;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelPort;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelError;
+        private System.Windows.Forms.TextBox textBoxDebug;
+    }
+}
+
---
+++ UartImage/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UartImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PortsRefresh();
+            int x = int.Parse(textBoxX.Text);
+            int y = int.Parse(textBoxY.Text);
+            bmp =new Bitmap(x,y);
+
+        }
+
+        private void PortsRefresh()
+        {
+            comboBox1.Items.Clear();
+            var ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+
+            if (ports.Length > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void ButtonConnect_Click(object sender, EventArgs e)
+        {
+            var port = comboBox1.Text;
+
+            if (port.Length>3)
+            {
+                var BaudRate = int.Parse(textBoxBaudRate.Text);
+
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                }
+
+                serialPort1.PortName = port;
+                serialPort1.BaudRate = BaudRate;
+                if (!serialPort1.IsOpen)
+                {
+                    serialPort1.Open();
+                }
+                else
+                {
+                    serialPort1.Close();
+                    serialPort1.Open();
+                }
+                TestPort();
+            }
+            else
+            {
+                MessageBox.Show("Выберите порт","Ahtung!");
+            }
+            
+        }
+
+        private void TestPort()
+        {
+            if (serialPort1.IsOpen)
+            {
+                toolStripStatusLabelPort.Text = "Порт " + serialPort1.PortName + " открыт";
+            }
+            else
+            {
+                toolStripStatusLabelPort.Text = "Порт " + serialPort1.PortName + " закрыт";
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            textBoxDebug.AppendText("Line " + lineNum.ToString() + " Size " + LineData.Count + "\r\n");
+
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+            TestPort();
+        }
+
+        private bool newLine = false;
+        private int lineNum = 0;
+        private int lastLineNum = 0;
+
+        List<byte> LineData=new List<byte>();
+        private int ReciveBytes = 0;
+        private int ReciveAllBytes = 0;
+        private Bitmap bmp;
+
+
+
+        private void SerialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            while (serialPort1.BytesToRead>0)
+            {
+                byte indata = (byte)serialPort1.ReadByte();
+                ReciveBytes++;                        
+
+                if (indata == 0 && ReciveBytes > 4)
+                {
+                    ReciveAllBytes += ReciveBytes;
+                    UpdateIMG();
+                    ReciveBytes = 0;
+                    LineData = new List<byte>();
+                }
+                if (ReciveBytes == 1)
+                {
+                    lineNum = indata;
+                }
+                if (ReciveBytes == 2)
+                {
+                    lineNum = lineNum * 256 + indata;
+                }
+                if (ReciveBytes > 2)
+                {
+                    LineData.Add(indata);
+                }
+            }
+        }
+
+        private void UpdateIMG()
+        {
+            try
+            {
+                if (lastLineNum>lineNum)
+                {
+                    pictureBox1.Image = (Image)bmp;
+                }
+                lastLineNum = lineNum;
+                toolStripStatusLabelError.Text = "Получено " + ReciveAllBytes.ToString() + " байт";
+                textBoxDebug.AppendText("Line " + lineNum.ToString() + " Size " + LineData.Count + "\r\n");
+                if (lineNum < bmp.Height)
+                {
+                    int px = 0;
+                    foreach (var color_val in LineData)
+                    {
+                        if (px < bmp.Width)
+                        {
+                            var col = Color.FromArgb(color_val, color_val, color_val);
+                            bmp.SetPixel(px, lineNum, col);
+                            px++;
+                        }
+                    }
+                    
+                }
+            }
+            catch (Exception)
+            {
+                toolStripStatusLabelError.Text= "Ошибка в функции UpdateIMG";
+            }
+        }
+
+        private void ButtonClear_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBoxX.Text);
+            int y = int.Parse(textBoxY.Text);
+            bmp = new Bitmap(x, y);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            PortsRefresh();
+        }
+    }
+}
